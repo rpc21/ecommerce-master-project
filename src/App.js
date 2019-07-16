25ed97975aa3d06f0fd963ef5a86163c04a8fd3f
Ryan Culhane <rpc21@duke.edu>
2019-07-16 17:46:00 -0400

    Implement firebase utils, now have ability to store authenticated users in firestore database

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -21,9 +21,30 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user});
-    })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapshot => {
+          /**
+           * We can check if the object exists using the .exists field
+           * To access the data we have to use .data() to see what the data looks like
+           * The id is on the snapshot itself
+           */
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data()
+            }
+          }, 
+          () => {
+            console.log(this.state);
+          });
+        });
+      }
+      else {
+        this.setState({currentUser: userAuth});
+      }
+    });
   }
 
   componentWillUnmount() {
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -10,6 +10,36 @@ const config = {
     storageBucket: "",
     messagingSenderId: "185331206015",
     appId: "1:185331206015:web:b303d7c52ed0d0d0"
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return; // only perform if a user signed in, not if they signed out
+    /**
+     * Firestore gives back either a QueryReference or QuerySnapshot
+     * Can be either document or collection versions
+     * ALWAYS returns these objects, even if not exists from the query
+     */
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get(); // use .get() to get the document snapshot
+
+    // Use document reference for CRUD (create, retrieve, update and delete) methods
+    if(!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+
+    }
+    return userRef;
 }
 
 firebase.initializeApp(config);
